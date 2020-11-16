c3007045859dd20e6c8cd586315eb5aaafb7827f
linktoaqeel <linktoaqeel@gmail.com>
2020-11-17 00:13:49 +0400

    Add project files.

---
+++ SampleBlazorWasm/ModelDTO/DashboardFilterDTO.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleBlazorWasm.ModelDTO
+{
+    public class DashboardFilterDTO
+    {
+        [Display(Name = "Department Id List")]
+        public string DepartmentIdList { get; set; }
+
+        [Display(Name = "Employee Id")]
+        public string EmployeeId { get; set; }
+
+        [Display(Name = "Duration")]
+        public string Daterange { get; set; }
+
+        [Display(Name = "Filter Set?")]
+        public bool IsFilterSet { get; set; }
+
+        public DashboardFilterDTO()
+        {
+            DepartmentIdList = string.Empty;
+            EmployeeId = string.Empty;
+            Daterange = DateTime.Now.Date.ToString("MM/01/yyyy") + " - " + DateTime.Now.Date.ToString("MM/dd/yyyy");
+            IsFilterSet = false;
+        }
+    }
+}
